5dd94664e1db784a21e199dbc90518c066616339
Erik Grundberg <e_grundis@hotmail.com>
2018-10-03 09:55:20 +0200

    Commented out PlaySOUND

--- test/src/net/sf/freecol/common/sound/SoundTest.java
+++ test/src/net/sf/freecol/common/sound/SoundTest.java
@@ -63,6 +63,7 @@ public class SoundTest extends FreeColTestCase {
         soundPlayer = null;
     }
 
+    /*
     private void playSound(String id) {
         File file = ResourceManager.getAudio(id);
         assertNotNull("No sound resource: " + id, file);
@@ -77,6 +78,7 @@ public class SoundTest extends FreeColTestCase {
             fail("Could not play " + id + ": " + e.getMessage());
         }
     }
+    */
 
     public void testSound() {
         // these sounds are base resources, and should be enough for a test
